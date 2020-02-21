f7903edb3bd2b77e3aa0842fd9986e24b4bb2d8c
unknown <id.lucas@outlook.com.br>
2020-02-21 00:38:55 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// PRIMEIROS COMANDOS A SER USADO NO PROJETO 
+// https://blog.rocketseat.com.br/react-native-autenticacao/
+
+import React from 'react';
+
+import Routes from './routes';
+
+const App = () => <Routes />;
+
+export default App;
+
+// ----------------------------------
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+// responsável pelas navegações entre as telas
+
+import { createStackNavigator } from 'react-navigation';
+import SignIn from './pages/signIn';
+import SignUp from './pages/signUp';
+import Main from './pages/main';
+
+const Routes = createStackNavigator({
+    SignIn,
+    SignUp,
+    Main,
+});
+
+export default Routes;
+// ----------------------------------
