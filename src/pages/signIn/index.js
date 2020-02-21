08198fe95b395b3ae9d264a5dcbb780274b957d3
unknown <id.lucas@outlook.com.br>
2020-02-21 08:47:47 -0300

    Continuacao

--- src/pages/signIn/index.js
+++ src/pages/signIn/index.js
@@ -7,7 +7,7 @@ import { StackActions, NavigationActions } from 'react-navigation';
 
 import api from '../../services/api';
 
-import{
+import {
     Container,
     Logo,
     Input,
@@ -18,12 +18,57 @@ import{
     SignUpLinkText,
 } from './styles';
 
+
 export default class SignIn extends Component {
-    render(){
-        return(
+
+
+    static navigationOptions = {
+        header: null,
+    };
+
+    state = {email: '', password: '', error: ''};
+    
+    handleEmailChange = (email) => {
+        this.setState({ email });
+    };
+
+    handlePasswordChange = (password) =>{
+        this.setState({ password });
+    };
+
+    handleCreateAccountPress = () => {
+        this.props.navigation.navigate('SignUp')
+    };
+
+
+    render() {
+        return (
             <Container>
                 <StatusBar hidden />
-                
+                <Logo source={require('../../images/consolidacao.png')} resizeMode="contain" />
+                <Input
+                    placeholder="Endereço de e-mail"
+                    value={this.state.email}
+                    onChangeText={this.handleEmailChange}
+                    autoCapitalize="none"
+                    autoCorrect="{false"
+                />
+                <Input
+                    placeholder="senha"
+                    value={this.state.password}
+                    onChangeText={this.handlePasswordChange}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    segureTextEntry
+                />
+                {this.state.error.length !== 0 && <ErrorMessage> {this.state.error} </ErrorMessage>}
+                <Button onPress={this.handleSignInPress}>
+                    <ButtonText>Entrar</ButtonText>
+                </Button>
+
+                <SignUpLink onPress={this.handleCreateAccountPress}>
+                    <SignUpLinkText>Criar conta grátis</SignUpLinkText>
+                </SignUpLink>
             </Container>
         );
     }
